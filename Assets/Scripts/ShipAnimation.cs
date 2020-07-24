71ac1465009348a71d7657e42e63d3a24fcc1e94
VilleSynkkanen <60285530+VilleSynkkanen@users.noreply.github.com>
2020-07-24 18:30:01 +0300

    Frigate & ironclad movement visuals

---
+++ Assets/Scripts/ShipAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShipAnimation : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator anim;
+    [SerializeField] SpriteRenderer sprite;
+    [SerializeField] float animationFadeTreshold;
+    [SerializeField] float animationSpeedCoefficient;
+
+    void Update()
+    {
+        float velocity = rb.velocity.magnitude;
+        anim.speed = animationSpeedCoefficient * velocity;
+        sprite.color = new Color(1, 1, 1, Mathf.Lerp(0, 1, velocity / animationFadeTreshold));
+    }
+}
