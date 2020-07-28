1985144ee057e9469370a945cba7b5270df451bb
VilleSynkkanen <60285530+VilleSynkkanen@users.noreply.github.com>
2020-07-28 23:02:22 +0300

    Muzzle flash particle effects

--- Assets/Scripts/ShotEffects.cs
+++ Assets/Scripts/ShotEffects.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class ShotEffects : Effects
+public class ShotEffects : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer[] effects0;
-    [SerializeField] SpriteRenderer[] effects1;
-    [SerializeField] SpriteRenderer[] effects2;
-    [SerializeField] SpriteRenderer[] effects3;
-    List<SpriteRenderer[]> effects = new List<SpriteRenderer[]>();
-    bool[] active = new bool[4];
-    float[] startTime = new float[4];
+    [SerializeField] ParticleSystem[] effects0;
+    [SerializeField] ParticleSystem[] effects1;
+    [SerializeField] ParticleSystem[] effects2;
+    [SerializeField] ParticleSystem[] effects3;
+    List<ParticleSystem[]> effects = new List<ParticleSystem[]>();
 
     private void Awake()
     {
@@ -21,35 +19,9 @@ public class ShotEffects : Effects
 
     public void Effect(int i)
     {
-        foreach (SpriteRenderer effect in effects[i])
+        foreach (ParticleSystem effect in effects[i])
         {
-            ActivateSprite(effect);
+            effect.Play();
         }
-        active[i] = true;
-        startTime[i] = Time.time;
-    }
-
-    public override void HandleEffects()
-    {
-        for (int i = 0; i < effects.Count; i++)
-        {
-            float delta = Time.time - startTime[i];
-            if (active[i])
-            {
-                foreach (SpriteRenderer effect in effects[i])
-                {
-                    effect.color = Color.Lerp(Visible, Invisible, delta * LerpSpeed);
-                    if (effect.color == Invisible)
-                    {
-                        active[i] = false;
-                    }
-                }
-            }
-        }
-    }
-    
-    void Update()
-    {
-        HandleEffects();
     }
 }
