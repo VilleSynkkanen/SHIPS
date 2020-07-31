3fdc52f33697a86336c1caa7914c241a5cd4da99
VilleSynkkanen <60285530+VilleSynkkanen@users.noreply.github.com>
2020-07-31 23:17:54 +0300

    Ship selection ui polish

---
+++ Assets/Scripts/MapTerrainAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTerrainAnimation : MonoBehaviour
+{
+    [SerializeField] float scaleAnimationLength;
+    [SerializeField] float moveAnimationLength;
+    [SerializeField] LeanTweenType scaleAnimationType;
+    [SerializeField] LeanTweenType moveAnimationType;
+
+    void Start()
+    {
+        LeanTween.scale(gameObject, Vector3.one, scaleAnimationLength).setEase(scaleAnimationType);
+        LeanTween.move(gameObject, Vector3.zero, moveAnimationLength).setEase(moveAnimationType);
+    }
+}
---
+++ Assets/Scripts/ShipSelectionHpSetter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShipSelectionHpSetter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI[] fronts;
+    [SerializeField] TextMeshProUGUI[] middles;
+    [SerializeField] TextMeshProUGUI[] rears;
+    [SerializeField] TextMeshProUGUI[] totals;
+
+    void Start()
+    {
+        SetHitpoints();
+    }
+
+    void SetHitpoints()
+    {
+        for(int i = 0; i < totals.Length; i++)
+        {
+            ShipData data = GameSettings.Instance.GetShipData((ShipType)i);
+            fronts[i].text = data.SegmentHp[0].ToString();
+            middles[i].text = data.SegmentHp[1].ToString();
+            rears[i].text = data.SegmentHp[2].ToString();
+            totals[i].text = data.MaxHp.ToString();
+        }
+    }
+}
